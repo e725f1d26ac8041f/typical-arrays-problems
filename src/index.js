7ea69fd92a43f2fb8481692b55d656173173e120
Anton Zakhar <e725f1d26ac8041f@gmail.com>
2021-02-12 21:35:51 +0300

    feat: implement max function

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ exports.min = function min (array = []) {
   return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if (array.length === 0) return 0;
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
